627c440070b91e1914cfc8f25ff8a74ac3bfbf07
minseok shin <alstjr7388@gmail.com>
2026-07-16 09:46:13 +0900

    뷰 프로젝트 테스트

---
+++ localhub/vue-project/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+const key = import.meta.env.VITE_API_KEY
+
+console.log(key)
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
